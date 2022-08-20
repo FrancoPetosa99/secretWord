216de8a102819d4f134d582b67484f0f2451af62
Franco Petosa Ayala <84604362+FrancoPetosa99@users.noreply.github.com>
2022-08-20 19:26:22 -0300

    updates

--- scripts/main2.js
+++ scripts/main2.js
@@ -2,17 +2,13 @@
     Script Name:   SECRET WORD GAME
     Customer:      CoderHouse
     Purpose:       
-    Preconditions: No preconditions needed
+    Preconditions: 
     Game Rules:
     Process PseudoCode:
-                    1. Extract a list of groups and get the group.  
-                    2. For each group found, get user information and load user information into the UserData object.
-                    3. Return UserData object to the calling function.
-    Return Array:  The following represents the array of information returned to the calling function.  This is a standardized response.
-                        Any item in the array at points 2 or above can be used to return multiple items of information.
-                        0 - Status: 'Success' or 'Error'
-                        1 - Message
-                        2 - User data object with list of users.
+        1.
+        2. 
+        3. 
+
     Date of Dev:   15/08/2022
     Last Rev Date: 15/08/2022
     Revision Notes:
@@ -36,13 +32,14 @@ const wrongColor = '#787C7E';
 class Game{
 
     //tiene como proposito crear instancias del juego por cada secretWord
-    constructor(rowList, secretWord){
-        this.rowList = rowList; //array
-        this.currentRow = 0; // index
+    constructor(secretWord){
+        this.rowList = Array.from(document.getElementsByClassName('row'));
+        this.currentRow = 0;
         this.secretWord = secretWord
         this.currentWord = '';
         this.lifeNumber = lifeNumber;
         this.isGameOver = false;
+        this.keyBoard = document.querySelectorAll('.keyWord');
     }
 
     deleteChar(){
@@ -91,13 +88,13 @@ class Game{
             const title = 'Level passed';
             const message = 'Do you want to continue?'
             const labelBtn = 'Okey'
-            displayMessage(title, message, labelBtn, resetGame)
+            displayMessage(title, message, labelBtn)
         }else if(this.lifeNumber -1 == 0){
             this.isGameOver = true;
             const title = 'Game Over';
             const message = 'You have lost'
             const labelBtn = 'Try Again'
-            displayMessage(title, message, labelBtn, resetGame)
+            displayMessage(title, message, labelBtn)
         }else{
             this.lifeNumber = this.lifeNumber - 1
             this.currentRow++;
@@ -128,11 +125,33 @@ class Game{
 
     checkWord(){
         //solo si se completaron todos los boxes de una fila
-        //recie en esa instancia se puede evaluar el resultado
+        //recien en esa instancia se puede evaluar el resultado
         if (this.currentWord.length == this.secretWord.length) {
             this.colorBoxesAndKeys();
             this.checkGameStatus();
         }
+    }
+
+    setKeyBoardToPlay(){
+        this.keyBoard.forEach(key => {
+            key.addEventListener('click', ()=>{
+                if(!this.isGameOver){
+    
+                    switch (key.id) {
+                        case 'Enter':
+                           this.checkWord();
+                        break;
+                        case 'Delete':
+                            this.deleteChar();
+                        break;
+        
+                        default:
+                            this.selectKey(key);
+                        break;
+                    }
+                }
+            })
+        })
     }
 }
 
@@ -141,10 +160,12 @@ class Spinner{
         this.spinner = document.getElementById('spinner')
     }
 
+    //muestra el spinner cuando se invoca este metodo del objeto
     show(){
         this.spinner.classList.remove('hide');
     }
 
+    //desaparecer el spinner cuando se invoca este metodo del objeto
     hide(){
         this.spinner.classList.add('hide');
     }
@@ -159,6 +180,9 @@ const keyWordList = document.querySelectorAll('.keyWord');
 const boardGame = document.getElementById('boardGame');
 const remainLifes = document.getElementById('lifeNumber');
 const howToPlayIcon = document.getElementById('howToPlayIcon');
+const modalTitle = document.getElementById('modalTitle');
+const modalText = document.getElementById('modalText');
+const modalBtn = document.getElementById('modalBtn'); 
 const modal = new bootstrap.Modal('#modal', {keyboard: false,backdrop: 'static'});
 const instructions = new bootstrap.Modal('#instructions', {keyboard: false,backdrop: 'static'});
 const spinner = new Spinner();
@@ -200,28 +224,18 @@ function buildGameBoard(secretWord){
     
 }    
 
-function displayMessage(title, message, btnLabel, callback){
+function displayMessage(title, message, btnLabel){
     //muestra un mensaje modal al usuario
     //los siguientes elementos son customizables:
         //el titulo del modal
         //el mensaje
         //el label del button para ejecutar la acción
 
-    const modalTitle = document.getElementById('modalTitle');
-    const modalText = document.getElementById('modalText');
-    const modalBtn = document.getElementById('modalBtn'); 
-
     modalTitle.textContent = title;
     modalText.textContent = message;
     modalBtn.textContent = btnLabel;
 
-    modal.show()
-
-    modalBtn.addEventListener('click', ()=>{
-        //primero cierra el modal y luego invoca la función de callback
-        modal.hide()
-        callback()
-    })
+    modal.show();
 }
 
 function resetGame(){
@@ -234,6 +248,12 @@ function resetGame(){
     main();
 }
 
+function getSecretWord(){
+    return fetch(`https://api.api-ninjas.com/v1/randomword?type=noun`)
+                .then((res) => checkMetaStatus(res))
+                .then((res) => res.json())
+                .then((data) => data.word.toUpperCase())
+}
 /* -------------------------------------------------------------------------- */
 
 
@@ -242,37 +262,12 @@ async function main(){
     
     try{
         spinner.show();
-        const secretWord = await fetch(`https://api.api-ninjas.com/v1/randomword?type=noun`)
-                        .then((res) => checkMetaStatus(res))
-                        .then((res) => res.json())
-                        .then((res) => res.word.toUpperCase())
+        const secretWord = await getSecretWord();
         spinner.hide();
         buildGameBoard(secretWord);
-        const rowList = Array.from(document.getElementsByClassName('row'));
-        const game = new Game(rowList,secretWord)
-
-        keyWordList.forEach(key => {
-
-            key.addEventListener('click', ()=>{
-        
-                if(!game.isGameOver){
-    
-                    switch (key.id) {
-                        case 'Enter':
-                           game.checkWord();
-                        break;
-                        case 'Delete':
-                            game.deleteChar();
-                        break;
-        
-                        default:
-                            game.selectKey(key);
-                        break;
-                    }
-                }
-            })
-        })
-
+        const game = new Game(secretWord);
+        game.setKeyBoardToPlay();
+       
     }catch(error){
         //atrapa cualquier error que haya podido suceder en el proceso del juego
         //Muestra un mensaje modal para indicarle al usuario que hubo un problema 
@@ -280,7 +275,7 @@ async function main(){
         const title = 'Ups!';
         const message = 'Something went wrong';
         const labelBtn = 'Try again';
-        displayMessage(title, message, labelBtn, resetGame);
+        displayMessage(title, message, labelBtn);
     }
 }
 main()
@@ -290,3 +285,8 @@ howToPlayIcon.addEventListener('click', ()=>{
     instructions.show();
 })
 
+modalBtn.addEventListener('click', ()=>{
+    //primero cierra el modal y luego invoca la función de callback
+    modal.hide();
+    resetGame();
+});
